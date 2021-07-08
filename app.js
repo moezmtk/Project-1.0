26f335f7ed4d2fc670f39c0d0f5b75cc117dae26
moezmtk <moezmtk@gmail.com>
2021-07-08 09:21:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -18,45 +18,65 @@ app.listen(port,()=>{
 })
 
 
-//ye5dem
+//work
 app.get('/firstUpper/:str1',(req,res)=>{
+    
+    //exemple : test
     let str2 = req.params.str1
     str2 = str2.charAt(0).toUpperCase() + str2.slice(1)
     res.json({result: str2})
     console.log(str2)
 
 })
-
-app.get('/deleteElement/:index',(req,res)=>{
+//work
+app.post('/deleteElement/:index',(req,res)=>{
     
-    
-    var array = [1,2,3,4]
-    
-    
+    //exemple = [1,2,3,4]
+   
+    let array = req.body
+    console.log(array)
     const index = req.params.index
     array.splice(index,1)
-    res.json({result:array})
+    res.json({result:array}) 
     console.log(array)
 })
-
-app.get('/deepClone',(req,res)=>{
+//work
+app.post('/deepClone',(req,res)=>{
     
-    const obj1 = { 
-        firstName: "James", 
-        lastName: "Bond", 
-        age: 9}
-
-
+    //const obj1 = {firstName: "James", lastName: "Bond", age: 9}
+    //exemple : {"firstName": "James", "lastName": "Bond", "age": 9}
+   
+    const obj1 = req.body  
     const obj2 = Object.assign({},obj1)
     res.json({result:obj2})
-    console.log(obj2)
+    console.log('obj1 = ',obj1)
+    console.log('obj2 = ',obj2)
 
 })
 
-app.get('/groupByProperty/:prop',(req,res)=>{
+//work
+app.get('/subString/:str4/:str5',(req,res)=>{
     
-    const array = [{name:'test'},{name:'test'},{name:'test1'},{name:'test12'},{name:'test12'}]
+    //exemple str4="test",str5="tes"
     
+    const str4 = req.params.str4
+    const str5 = req.params.str5
+    if(str4.includes(str5)){
+        console.log(str5 ,'sub string of ', str4)
+        res.json({status:true})
+    }else{
+        console.log(str5 ,'not sub string of ', str4)
+        res.json({status:false})
+    }
+
+})
+
+//work
+app.post('/groupByProperty/:prop',(req,res)=>{
+    
+    //const array = [{name:'test'},{name:'test'},{name:'test1'},{name:'test12'},{name:'test12'}]
+    //exemple : [{"name":"test"},{"name":"test"},{"name":"test1"},{"name":"test12"},{"name":"test12"}]
+    const array = req.body
     
     const prop = req.params.prop
     function groupBy(objectArray, property) {
@@ -73,19 +93,7 @@ app.get('/groupByProperty/:prop',(req,res)=>{
     res.json({result:result})
     console.log(result)
 })
-//ye5dem
-app.get('/subString/:str4/:str5',(req,res)=>{
-    const str4 = req.params.str4
-    const str5 = req.params.str5
-    if(str4.includes(str5)){
-        console.log(str5 ,'sub string of ', str4)
-        res.json({status:true})
-    }else{
-        console.log(str5 ,'not sub string of ', str4)
-        res.json({status:false})
-    }
 
-})
 
 
 
