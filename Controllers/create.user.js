8f990c49c2e95cbaaa827788d3f4925d2695491e
moez barhoumi <moezmtk@gmail.com>
2021-07-09 17:48:07 +0100

    commit

--- Controllers/create.user.js
+++ Controllers/create.user.js
@@ -5,5 +5,6 @@ const connected = require('../Config/db.config')
 //exemple : { "frist_name": "moez", "last_name" : "barhoumi", "email" : "moez@gmail.com", "phone" : "20189195", "status" : "1" }
 
 // creat new user
-const User = User.create({ frist_name: req.body.frist_name, last_name : req.body.last_name, email : req.body.email, phone : req.body.phone, status : req.body.status })
+const User = User.create({ frist_name: req.body.frist_name , last_name : req.body.last_name , email : req.body.email , phone : req.body.phone , status : req.body.status })
    
+module.exports = User
