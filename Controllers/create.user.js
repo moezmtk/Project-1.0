f21f40067e734328cda64bc76d9845eff09b84c3
moez barhoumi <moezmtk@gmail.com>
2021-07-09 15:33:46 +0100

    commit

--- Controllers/create.user.js
+++ Controllers/create.user.js
@@ -7,7 +7,7 @@ const connected = require('../Config/db.config')
 
 // creat new user
 exports.createUser = (req,res)=>{
-    connected.query('INSERT INTO user SET ? ',req,(err,res)=>{
+    connected.query('INSERT INTO user SET ?',req,(err,res)=>{
         if(err){
             console.log('Error while inserting data');
             res.json({error:err});
