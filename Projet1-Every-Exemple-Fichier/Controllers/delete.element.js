48ff0e047c21bbeccf9a0a0af99c9c8ee04e4851
moez barhoumi <moezmtk@gmail.com>
2021-07-09 11:48:45 +0100

    Add files via upload

---
+++ Projet1-Every-Exemple-Fichier/Controllers/delete.element.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const route = require('../Router/router')
+
+//const array = [1,2,3,4,5]
+exports.deleteElement = (req,res)=>{
+    //var array = [1,2,3,4]
+    let array = req.body
+    console.log(array)
+    const index = req.params.index
+    array.splice(index,1)
+    res.json({result:array}) 
+    console.log(array)
+}
