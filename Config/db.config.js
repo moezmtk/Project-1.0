65da6c8fe1304858bdb4041c0870eb2199ae079b
moez barhoumi <moezmtk@gmail.com>
2021-07-09 15:05:39 +0100

    commit main

--- Config/db.config.js
+++ Config/db.config.js
@@ -26,7 +26,7 @@ const connected = new Sequelize('base', 'root', '', {
 
 //testing connection 
 try {
-    await connected.authenticate();
+    connected.authenticate();
     console.log('Connection has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
--- Controllers/create.user.js
+++ Controllers/create.user.js
@@ -3,6 +3,7 @@ const express = require('express')
 const connected = require('../Config/db.config')
 
 
+//exemple : { "frist_name": "moez", "last_name" : "barhoumi", "email" : "moez@gmail.com", "phone" : "20189195", "status" : "1" }
 
 // creat new user
 exports.createUser = (req,res)=>{
