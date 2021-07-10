4539203e629d5be3e6952939301822050d70d574
moez barhoumi <moezmtk@gmail.com>
2021-07-10 09:21:53 +0100

    commit

--- Controllers/create.user.js
+++ Controllers/create.user.js
@@ -1,10 +1,10 @@
 
 const express = require('express')
-
+const User = require('../models/user.model')
 
 //exemple : { "first_name": "moez", "last_name" : "barhoumi", "email" : "moez@gmail.com", "phone" : "20189195", "status" : "1" }
 
 // creat new user
-const User = User.create({ first_name: req.body.first_name , last_name : req.body.last_name , email : req.body.email , phone : req.body.phone , status : req.body.status })
+User = User.create({ first_name: req.body.first_name , last_name : req.body.last_name , email : req.body.email , phone : req.body.phone , status : req.body.status })
    
 module.exports = User
