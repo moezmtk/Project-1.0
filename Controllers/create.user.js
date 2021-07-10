a64d77fffe65920e87328b75e5ee7a1f7085df9d
moez barhoumi <moezmtk@gmail.com>
2021-07-10 10:57:08 +0100

    commit

--- Controllers/create.user.js
+++ Controllers/create.user.js
@@ -4,7 +4,23 @@ const User = require('../models/user.model')
 
 //exemple : { "first_name": "moez", "last_name" : "barhoumi", "email" : "moez@gmail.com", "phone" : "20189195", "status" : "1" }
 
-// creat new user
+/*// creat new user
 User = User.create({ first_name: req.body.first_name , last_name : req.body.last_name , email : req.body.email , phone : req.body.phone , status : req.body.status })
    
-module.exports = User
+module.exports = User*/
+
+exports.createUser = (req, res) => {    
+    const { first_name,last_name, email, phone, status } = req.body
+      User.create({
+        first_name,
+        last_name,
+        email,
+        phone,
+        status  
+      })
+       res.json(user)
+      
+    
+  }
+
+
